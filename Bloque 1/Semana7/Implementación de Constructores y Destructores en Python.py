3fb754e4a32a655f6924c5713d4c9a75cf39b79e
Vely2004 <nn.ceminarioa@uea.edu.ec>
2025-01-26 11:15:30 -0500

    POO_01

--- Bloque 1/Semana7/Implementación de Constructores y Destructores en Python.py
+++ Bloque 1/Semana7/Implementación de Constructores y Destructores en Python.py
@@ -1,23 +1,24 @@
- class libro:
-     def _init_(self, titulo, autor, anio_publicacion):
-         ''' Constructor: Inicializa los atributos del objeto.
+class Libro:
+    def __init__(self, titulo, autor, anio_publicacion):
+        ''' Constructor: Inicializa los atributos del objeto.
         :param titulo: Título del libro.
         :param autor: Autor del libro.
         :param anio_publicacion: Año de publicación del libro.'''
-        self.titulo= titulo
-        self.autor= autor
-        self.anio_publicacion= anio_publicacion
- print(f"Libro '{self.titulo}' creado.")
- def mostrar_info(self):
-     '''sirve para mostrar la información del libro.'''
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
+        print(f"Libro '{self.titulo}' creado.")  # Esto se ejecuta cuando el objeto es creado
+
+    def mostrar_info(self):
+        '''Sirve para mostrar la información del libro.'''
         print(f"Título: {self.titulo}")
         print(f"Autor: {self.autor}")
         print(f"Año de publicación: {self.anio_publicacion}")
 
+    def __del__(self):
+        ''' Destructor: Simula la limpieza de recursos al eliminar el objeto.'''
+        print(f"Libro '{self.titulo}' ha sido destruido.")  # Esto se ejecuta cuando el objeto es destruido
 
- def __del__(self):
-     '''' Destructor: Simula la limpieza de recursos al eliminar el objeto.'''
-     print(f"Libro '{self.titulo}' ha sido destruido.")
 # Crear una instancia del objeto
 mi_libro = Libro("Cien Años de Soledad", "Gabriel García Márquez", 1967)
 
